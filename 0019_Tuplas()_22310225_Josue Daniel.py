ce751431be26bc3b900237815f8a14a431f6bf64
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-20 23:12:24 -0600

    0019_tuplas
    op

---
+++ 0019_Tuplas()_22310225_Josue Daniel.py
@@ -0,0 +1,24 @@
+#Tuplas y sus Funciones
+
+#Ejemplos
+
+#tupla = ('rojo', 'azul', 'verde', 'amarillo')
+#print(tupla[0])
+
+#tupla = (10, 15, 20, 'El resultado de la operación es:')
+#print(tupla[3], tupla[2] + tupla[0] * tupla[1] / tupla[0])
+
+
+#Ejercicio
+
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+print(colores[1])
+
+
+numero = (10, 1, 5, 11,'El resultado de la operacion es: ')
+
+operacion = (numero[4],numero[0]-numero[1]+numero[2]+numero[3])
+
+print(operacion)
