882a39ca39ebc68d1905735b9faa633741f89a74
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-21 00:55:55 -0600

    0022

---
+++ 0022_Condicionales - IF - ELSE - _22310225_Josue Daniel.py
@@ -0,0 +1,21 @@
+#El condicional IF ELSE de Python
+
+
+#Corrige el siguiente condicional if else.
+#color = rojo
+
+#else color == rojo
+#Print "El color es rojo."
+#if color != rojo
+#Print "El color no es rojo."
+
+
+
+color = "rojo"
+
+
+if color != "rojo":
+    print ("El color no es rojo.")
+else: 
+    print ("El color es rojo.")
+
