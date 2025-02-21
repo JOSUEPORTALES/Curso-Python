8ff190e8c0be1eb2f4bf8ec2db31856ab05e522c
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-20 23:40:25 -0600

    0021

---
+++ 0021_Condicionales - IF - _22310225_Josue Daniel.py
@@ -0,0 +1,38 @@
+#El condicional IF de Python
+
+
+#Cambia el operador para que la condición sea True.
+
+num1 = 15
+num2 = 20
+
+if num1 < num2:
+	print('La condicion se cumple.')
+
+#Cambia el operador para que la condición sea True.
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('La condicion se cumple.')
+
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
