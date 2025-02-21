1ef345bc13a3a2a7eea47d116e7309a7ed5c5efa
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-21 12:52:45 -0600

    0024

---
+++ 0024_Comprobar datos en listas_22310225_Josue Daniel.py
@@ -0,0 +1,13 @@
+#Buscar datos en listas y tuplas Python
+
+
+#Ejercicios
+
+colores = ('azul','amarillo','rojo', 'violeta')
+
+colorE = input('Introduce un color: ')
+
+if colorE in colores:
+    print('El color esta en la tupla')
+else:
+    print('El color no esta en la tupla')
