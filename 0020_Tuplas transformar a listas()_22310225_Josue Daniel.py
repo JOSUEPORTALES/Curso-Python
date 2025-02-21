2c0d70d943edbe104bb7a0ea8187f98ed2257c0d
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-20 23:26:24 -0600

    0020

---
+++ 0020_Tuplas transformar a listas()_22310225_Josue Daniel.py
@@ -0,0 +1,22 @@
+#Cómo convertir una lista en una tupla\
+
+#Ejemplo
+
+#lista = ['rojo', 'azul', 'verde', 'amarillo']
+#tupla = tuple(lista)
+#print(tupla)
+
+#lista = ['rojo', 'azul', 'verde', 'amarillo']
+#tupla = tuple(lista)
+#print(type(tupla))
+
+#Convierte la siguiente lista en una tupla y asegúrate que se haya
+#en tupla correctamente imprimiendo
+#en la consola el tipo de elemento que es.
+
+
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colorest = tuple(colores)
+print(type(colorest))
