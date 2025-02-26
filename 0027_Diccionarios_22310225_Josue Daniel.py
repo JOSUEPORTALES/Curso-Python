0ddd1047422dc30ff984dfca05c1c277a151fe3d
JOSUEPORTALES <143145186+JOSUEPORTALES@users.noreply.github.com>
2025-02-26 12:51:53 -0600

    0027

--- 0027_Diccionarios_22310225_Josue Daniel.py
+++ 0027_Diccionarios_22310225_Josue Daniel.py
@@ -4,7 +4,7 @@
 #Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio
 #con presentación en un print(). El resultado será esto en la consola:
 #El modelo Corsair K55 RGB cuesta 59,99 $.
-
+"""
 teclado1 = {
 	'Categoría': 'Teclados',
 	'Modelo': 'HyperX Alloy FPS Pro',
@@ -20,10 +20,12 @@ teclado2['Modelo']
 teclado2['Precio']
 
 """
+"""
 Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio
 con presentación en un print().
 El resultado será esto en la consola:
 
+"""
 """
 #print ('El modelo' ,teclado2['Modelo'],'cuesta' ,teclado2['Precio'])
 
@@ -31,3 +33,43 @@ El resultado será esto en la consola:
 #que muestre esto en la consola:
 for x in teclado2:
     print(x,'=',teclado2[x])
+"""
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+
+
+
+    
+#Metodos con diccionarios
+
+"""
+Elimina el diccionario teclado1 entero .
+De teclado2 elimina las claves 'Categoría' y 'Precio'.
+Muestra la última clave ('Modelo') que queda en la consola.
+"""
+del teclado1
+del teclado2['Categoría']
+del teclado2['Precio']
+print(teclado2)
+
+
+
+
+
+
+
+
+
+
+
+    
